615406f80b16c8ebcc7167f8eac7bf9f98cedcf8
mayayannnn <S.mayayann.2010@gmail.com>
2024-04-03 16:27:05 +0900

    dog

--- LINE_bot.py
+++ LINE_bot.py
@@ -47,11 +47,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text.find("いぬ") >= 0:
+    if event.message.text in "いぬ":
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=event.message.text + "わん"))
-            print(event.message.text)
     else:
         line_bot_api.reply_message(
             event.reply_token,
