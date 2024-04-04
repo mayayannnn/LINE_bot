5d1a2f6f2b1f52fde92c42f2a80326f562ae982e
mayayannnn <S.mayayann.2010@gmail.com>
2024-04-04 15:12:01 +0900

    image

--- LINE_bot.py
+++ LINE_bot.py
@@ -54,7 +54,7 @@ def handle_message(event):
     if "画像" in event.message.text:
             line_bot_api.reply_message(
                 event.reply_token, {
-                "type": 'image',
+                type: 'image',
                 "originalContentUrl": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhnhnvT4_2cxoFHgNG2slYCqxy6PTr5L_hrgN6lvm_fFNvtp_1UXELKAD1A3rRY9kgb6yCHKnTH7tTG9QJIrs0ZCnLDpoHaWRUiHWm03l9lbeooMzw9nZqt8PVDFJcUhxu8qu-I4H2HnN8/s800/kid_job_boy_programmer.png", 
                 "previewImageUrl":  "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhnhnvT4_2cxoFHgNG2slYCqxy6PTr5L_hrgN6lvm_fFNvtp_1UXELKAD1A3rRY9kgb6yCHKnTH7tTG9QJIrs0ZCnLDpoHaWRUiHWm03l9lbeooMzw9nZqt8PVDFJcUhxu8qu-I4H2HnN8/s800/kid_job_boy_programmer.png"
             })
