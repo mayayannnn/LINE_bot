8568d8402f9fca8a862a23c7fc53a3f1cd9c4285
mayayannnn <S.mayayann.2010@gmail.com>
2024-04-05 17:18:26 +0900

    image

--- LINE_bot.py
+++ LINE_bot.py
@@ -19,6 +19,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
+import requests
 
 app = Flask(__name__)
 
@@ -27,6 +28,7 @@ YOUR_CHANNEL_SECRET =  os.getenv("YOUR_CHANNEL_SECRET")
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+headers = {'Authorization': 'Bearer ' + YOUR_CHANNEL_SECRET}
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -50,15 +52,13 @@ def handle_message(event):
     if "いぬ" in event.message.text:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=event.message.text + "わん"))
+                TextSendMessage(text=event.message.text + "わんこ"))
     elif "画像" in event.message.text:
-             line_bot_api.reply_message(
-                event.reply_token,   {
-                  "type": "image",
-                  "originalContentUrl": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhnhnvT4_2cxoFHgNG2slYCqxy6PTr5L_hrgN6lvm_fFNvtp_1UXELKAD1A3rRY9kgb6yCHKnTH7tTG9QJIrs0ZCnLDpoHaWRUiHWm03l9lbeooMzw9nZqt8PVDFJcUhxu8qu-I4H2HnN8/s800/kid_job_boy_programmer.png",
-                  "previewImageUrl": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhnhnvT4_2cxoFHgNG2slYCqxy6PTr5L_hrgN6lvm_fFNvtp_1UXELKAD1A3rRY9kgb6yCHKnTH7tTG9QJIrs0ZCnLDpoHaWRUiHWm03l9lbeooMzw9nZqt8PVDFJcUhxu8qu-I4H2HnN8/s800/kid_job_boy_programmer.png"
-                    }
-                )
+            message = 'Write Your Message'
+            image = 'test.png'  # png or jpg
+            payload = {'message': message}
+            files = {'imageFile': open(image, 'rb')}
+            r = requests.post(line_bot_api, headers=headers, params=payload, files=files,)
     else:
         line_bot_api.reply_message(
             event.reply_token,
