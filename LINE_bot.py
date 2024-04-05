ff2a912335a54be06580b3a5d11d7ca00a88c51f
mayayannnn <S.mayayann.2010@gmail.com>
2024-04-05 17:04:28 +0900

    image

--- LINE_bot.py
+++ LINE_bot.py
@@ -52,7 +52,7 @@ def handle_message(event):
     if "いぬ" in event.message.text:
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text=event.message.text + "わん"))
+                TextSendMessage(text=event.message.text + "わんこ"))
     elif "画像" in event.message.text:
             line_bot_api.reply_message(
                 event.reply_token,
