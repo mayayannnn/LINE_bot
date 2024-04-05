d4603c675acfea80c4caebd4ea50c09f232e41ef
mayayannnn <S.mayayann.2010@gmail.com>
2024-04-05 16:51:15 +0900

    image

--- LINE_bot.py
+++ LINE_bot.py
@@ -54,7 +54,7 @@ def handle_message(event):
                 event.reply_token,
                 TextSendMessage(text=event.message.text + "わん"))
     if "画像" in event.message.text:
-            image = 'C:/Users/Public/google.jpg'  # png or jpg を指定
+            image = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhnhnvT4_2cxoFHgNG2slYCqxy6PTr5L_hrgN6lvm_fFNvtp_1UXELKAD1A3rRY9kgb6yCHKnTH7tTG9QJIrs0ZCnLDpoHaWRUiHWm03l9lbeooMzw9nZqt8PVDFJcUhxu8qu-I4H2HnN8/s800/kid_job_boy_programmer.png'  # png or jpg を指定
             files = {'imageFile': open(image, 'rb')}
             r = requests.post(url, headers=headers, files=files,)
     else:
