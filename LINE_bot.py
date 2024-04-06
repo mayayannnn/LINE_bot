caa80fd652bb97f5b442844e48c61d7f721aa611
mayayannnn <S.mayayann.2010@gmail.com>
2024-04-06 09:58:25 +0900

    sticker

--- LINE_bot.py
+++ LINE_bot.py
@@ -58,9 +58,9 @@ def handle_message(event):
                 event.reply_token,
                 ImageSendMessage(original_content_url='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhnhnvT4_2cxoFHgNG2slYCqxy6PTr5L_hrgN6lvm_fFNvtp_1UXELKAD1A3rRY9kgb6yCHKnTH7tTG9QJIrs0ZCnLDpoHaWRUiHWm03l9lbeooMzw9nZqt8PVDFJcUhxu8qu-I4H2HnN8/s800/kid_job_boy_programmer.png',preview_image_url='https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhnhnvT4_2cxoFHgNG2slYCqxy6PTr5L_hrgN6lvm_fFNvtp_1UXELKAD1A3rRY9kgb6yCHKnTH7tTG9QJIrs0ZCnLDpoHaWRUiHWm03l9lbeooMzw9nZqt8PVDFJcUhxu8qu-I4H2HnN8/s800/kid_job_boy_programmer.png'))
     elif "スタンプ" in event.massage.text:
-            line_bot_api.reply_message(
+           line_bot_api.reply_message(
                 event.reply_token,
-                StickerSendMessage( package_id='1',sticker_id='1')
+                StickerSendMessage(package_id="446",sticker_id="1988")
             )
     else:
         line_bot_api.reply_message(
